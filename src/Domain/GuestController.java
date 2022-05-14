27724af163878df285512747c117408df6e5e141
Eldar <eldark@post.bgu.ac.il>
2022-05-14 14:46:28 +0300

    finish implementation of methods and DB start testing

--- src/Domain/GuestController.java
+++ src/Domain/GuestController.java
@@ -5,17 +5,19 @@ import java.util.ArrayList;
 
 public class GuestController {
 
-    public void RequestLogin(String username, String password) {
+    public boolean RequestLogin(String username, String password) {
         AzureDB myDB = new AzureDB();
         String sql = "Select username,email from Users where username="+"'"+username+"'"+" and"+" password="+"'"+password+"'";
         ArrayList<ArrayList<String>> res = myDB.SelectAzureSQL(sql);
 
         if (res.isEmpty() ) {
             System.out.println("User Does Not Exist or Password is Wrong");
+            return false;
         }
         else {
             System.out.println(res);
             System.out.println("Username : " + res.get(0).get(0) + " Email: " + res.get(0).get(1) + " Connected");
+            return true;
         }
 
     }
