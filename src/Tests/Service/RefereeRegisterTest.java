a585e17f5f8802941be5acfebbff8069b0be363f
Eldar <eldark@post.bgu.ac.il>
2022-05-14 16:32:11 +0300

    start testing

--- src/Tests/Service/RefereeRegisterTest.java
+++ src/Tests/Service/RefereeRegisterTest.java
@@ -1,22 +1,31 @@
 package Service;
 
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
 
 import static org.junit.Assert.*;
 
 public class RefereeRegisterTest {
     private RefereeRegister refereeRegister;
 
-    @Test
+    @BeforeAll
     public void setup() {
         System.out.println("Instantiating Referee Register");
-        this.refereeRegister = RefereeRegister();
+        this.refereeRegister = new RefereeRegister();
     }
+
     @Test
-    public void shouldNotExist() {
+    public void refereeNotExist() {
         boolean flag=this.refereeRegister.RegisterReferee("Jo", "mama", "123");
         assertFalse(flag);
     }
+
+    @Test
+    public void gameNotExist() {
+        boolean flag=this.refereeRegister.RegisterReferee("Jo", "mama", "123");
+        assertFalse(flag);
+    }
+
     @Test
     public void shouldExist() {
         boolean flag=this.refereeRegister.RegisterReferee("david", "654321", "123");
