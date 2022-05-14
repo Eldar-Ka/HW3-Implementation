16e1a131defbe070a8f07e9fad2cde41fbb7ce77
Eldar <eldark@post.bgu.ac.il>
2022-05-14 14:50:59 +0300

    finish implementation of methods and DB start testing

---
+++ src/Tests/Service/LoginProcedureTest.java
@@ -0,0 +1,13 @@
+package Service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginProcedureTest {
+
+    @Test
+    public void requestLogin() {
+        return;
+    }
+}
