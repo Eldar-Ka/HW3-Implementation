a8fe17fb533d96a1409d69f69e25d35d772a2e7b
Yarin Bnyamin <yarinbnyamin@post.bgu.ac.il>
2022-05-15 22:57:16 +0300

    Controller testing

--- src/Tests/Domain/GuestControllerTest.java
+++ src/Tests/Domain/GuestControllerTest.java
@@ -1,12 +1,36 @@
 package Domain;
 
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
-import static org.junit.Assert.*;
 
 public class GuestControllerTest {
+    private GuestController guestController;
+
+
+    @BeforeAll
+    public void setup() {
+        System.out.println("Instantiating Login Procedure");
+        this.guestController=new GuestController();
+    }
 
     @Test
-    public void requestLogin() {
+    @DisplayName("this test should not login because the user does not exist")
+    public void shouldNotExist() {
+        boolean flag=this.guestController.RequestLogin("Jo", "mama");
+        assertFalse(flag);
+    }
+    @Test
+    @DisplayName("this test should login an existing member")
+    public void shouldExist() {
+        boolean flag=this.guestController.RequestLogin("david", "654321");
+        assertFalse(flag);
+    }
+    @Test
+    @DisplayName("this test should not login an existing member because of a wrong password")
+    public void wrongPass() {
+        boolean flag=this.guestController.RequestLogin("david", "123");
+        assertFalse(flag);
     }
 }
