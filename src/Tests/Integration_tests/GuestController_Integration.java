e11a6f7209395814bc70990ceaae6ba974a8f648
Eldar <eldark@post.bgu.ac.il>
2022-05-24 20:29:25 +0300

    tests done

---
+++ src/Tests/Integration_tests/GuestController_Integration.java
@@ -0,0 +1,48 @@
+package Integration_tests;
+
+import Domain_tests.*;
+import Service_tests.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class GuestController_Integration {
+    private GuestControllerTest guestControllerTest = new GuestControllerTest();
+    private LoginProcedureTest realLoginTest = new LoginProcedureTest();
+
+//    @BeforeAll
+//    public void setup() {
+//        System.out.println("Initiating Guest Controller Integration Tests");
+//        guestControllerTest = new GuestControllerTest();
+//    }
+
+    @Test
+    @DisplayName("First part of integration testing - going through first stub")
+    public void stubRequestLogin() {
+        /*
+        First stage of integration testing - going through the first stub.
+        using the first unit test.
+         */
+        guestControllerTest.shouldExist();
+        guestControllerTest.shouldNotExist();
+        guestControllerTest.userNotExist();
+        guestControllerTest.wrongPass();
+    }
+
+    @Test
+    @DisplayName("Second part of integration testing - going through real function")
+    public void RequestLogin() {
+        /*
+        Second stage of integration testing - going through real function since we
+        do not have any other sub-functions under the Request Login function
+        (going through database now)
+         */
+        realLoginTest.shouldExist();
+        realLoginTest.shouldNotExist();
+        realLoginTest.wrongPass();
+    }
+
+
+}
